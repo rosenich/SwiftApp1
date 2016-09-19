8aa7d8eb257d6adacfd16bc2ec152266e40772df
firstcommit <rosenich@badinhs.org>
2016-09-19 08:19:45 -0400

    first commit

--- swift app 2/ViewController.swift
+++ swift app 2/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    theLabel.text="Now Cooler!"
+        
+        self.view.backgroundColor=UIColor.darkGray
     }
 
     override func didReceiveMemoryWarning() {
